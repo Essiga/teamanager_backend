ab6cfb86285d7ee1e7613d3dee0ad5044dbd9cc2
inderro1@students.zhaw.ch <inderro1@students.zhaw.ch>
2022-09-27 10:27:37 +0300

    Database schema done

--- src/domain/Tea.ts
+++ src/domain/Tea.ts
@@ -1,29 +1,29 @@
-import { RandomUUIDOptions } from "crypto";
+import { PriceAtDate } from "./value-objects/PriceAtDate";
 import { SubType } from "./value-objects/SubType";
 import { TeaType } from "./value-objects/TeaType";
 import { UUID } from "./value-objects/UUID";
 
 export class Tea{
-    readonly id: UUID;
+    readonly id: string;
     readonly name: string;
     readonly type: TeaType;
     readonly subTypes: SubType[];
-    readonly currentPrice: number;
     readonly amount: number;
     readonly link: string;
     readonly vendor: string;
     readonly year: number;
+    readonly priceHistory: PriceAtDate[];
 
-    constructor(id: UUID, name: string, type: TeaType, subTypes: SubType[], currentPrice: number, amount: number, link: string, vendor: string, year: number){
+    constructor(id: string, name: string, type: TeaType, subTypes: SubType[], amount: number, link: string, vendor: string, year: number, priceHistory: PriceAtDate[]){
         this.id = id;
         this.name = name;
         this.type = type;
         this.subTypes = subTypes;
-        this.currentPrice = currentPrice;
         this.amount = amount;
         this.link = link;
         this.vendor = vendor;
         this.year = year;
+        this.priceHistory = priceHistory;
     }
 
  
