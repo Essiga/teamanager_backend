a257ac7487802274d340b8a76a2d601da554f130
inderro1@students.zhaw.ch <inderro1@students.zhaw.ch>
2022-09-27 14:50:29 +0300

    creat new branch to install and prepare express

--- src/domain/Tea.ts
+++ src/domain/Tea.ts
@@ -1,7 +1,7 @@
 import { PriceAtDate } from "./value-objects/PriceAtDate";
 import { SubType } from "./value-objects/SubType";
 import { TeaType } from "./value-objects/TeaType";
-import { Tea } from "@prisma/client";
+//import { Tea } from "@prisma/client";
 import { UUID } from "./value-objects/UUID";
 
 export class Tea{
@@ -15,18 +15,18 @@ export class Tea{
     readonly subTypes?: SubType[];
     readonly priceHistory?: PriceAtDate[];
 
-    // constructor(id: string, name: string, type: TeaType, amount: number, link: string, vendor: string, year: number, subTypes?: SubType[], priceHistory?: PriceAtDate[]){
-    //     this.id = id;
-    //     this.name = name;
-    //     this.type = type;
-    //     this.amount = amount;
-    //     this.link = link;
-    //     this.vendor = vendor;
-    //     this.year = year;
-    //     this.subTypes = subTypes;
-    //     this.priceHistory = priceHistory;
-    // }
-
+     constructor(id: string, name: string, type: TeaType, amount: number, link: string, vendor: string, year: number, subTypes?: SubType[], priceHistory?: PriceAtDate[]){
+         this.id = id;
+        this.name = name;
+         this.type = type;
+        this.amount = amount;
+        this.link = link;
+         this.vendor = vendor;
+         this.year = year;
+        this.subTypes = subTypes;
+        this.priceHistory = priceHistory;
+    }
+/* 
     constructor(tea: Tea){
         this.id = tea.id;
         this.name = tea.name;
@@ -38,6 +38,6 @@ export class Tea{
         this.subTypes = tea.subTypes;
         this.priceHistory = tea.priceHistory;
     }
- 
+  */
 
 }
