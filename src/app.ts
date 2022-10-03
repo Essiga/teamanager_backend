adb854ab3c5811c188e6162bb263a38b5ddcc176
prescherm <m.prescher@gantner-instruments.com>
2022-10-03 16:34:57 +0300

    added express router + some api definitions

--- src/app.ts
+++ src/app.ts
@@ -28,33 +28,40 @@ const vesselService: IVesselService = new VesselService(vesselRepository);
 const sessionRepository: ISessionRepository = new SessionRepository();
 const sessionService: ISessionService = new SessionService(sessionRepository);
 
-const expressOasGenerator = require('express-oas-generator');
+import path from 'path';
 
 const app = express();
-app.use(express.json());
-const port = 3000;
+const router = express.Router();
 
-expressOasGenerator.init(app, {});
+router.use(express.json());
 
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'Tea Manager',
+        version: '1.0.0',
+        description: 'A tea manager to manage teas, YES!'
+    },
+    servers: [{url: '/api'}]
+}
+
 const options = {
     failOnErrors: true,
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Tea Manager',
-            version: '1.0.0',
-        },
-    },
-    apis: ['./src/app.ts'], // files containing annotations
+    definition: swaggerDefinition,
+    apis: [
+        path.join(__dirname, '/app.ts'),
+        path.join(__dirname, '/api-schemas/yaml/*.yaml'),
+        path.join(__dirname, '/api-schemas/json/*.json')
+    ],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
-app.use('/api-myDocs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.get('/api-mySpecs', (req, res) => res.json(swaggerSpec))
+router.get('/api-specs', (req, res) => res.json(swaggerSpec))
 
 /**
  * @openapi
@@ -67,7 +74,7 @@ app.get('/api-mySpecs', (req, res) => res.json(swaggerSpec))
  *       500:
  *         description: Internal server error
  */
-app.post("/addTea", (req, res) => {
+router.post("/addTea", (req, res) => {
     addTeaService.addTea(req.body as Tea).then(
         () => {
             res.sendStatus(200);
@@ -90,9 +97,9 @@ app.post("/addTea", (req, res) => {
  *             schema:
  *               type: array
  *               items:
- *                 $ref: '#/api-schemas/Tea'
+ *                 $ref: '/api-schemas/json/Tea#'
  */
-app.get("/viewAllTeas", (req, res) => {
+router.get("/viewAllTeas", (req, res) => {
     viewTeaService.viewAllTeas().then(
         (data) => {
             res.send(data);
@@ -102,7 +109,7 @@ app.get("/viewAllTeas", (req, res) => {
         });
 });
 
-app.post("/addVessel", (req, res) => {
+router.post("/addVessel", (req, res) => {
     vesselService.addVessel(req.body as Vessel).then(
         () => {
             res.sendStatus(200);
@@ -112,7 +119,7 @@ app.post("/addVessel", (req, res) => {
         });
 });
 
-app.get("/viewAllVessels", (req, res) => {
+router.get("/viewAllVessels", (req, res) => {
     vesselService.viewAllVessels().then(
         (data) => {
             res.send(data);
@@ -123,7 +130,7 @@ app.get("/viewAllVessels", (req, res) => {
 })
 
 //TODO: Get tea and session info from request and auto calculate price
-app.post("/addSession", (req, res) => {
+router.post("/addSession", (req, res) => {
     sessionService.addSession(req.body as Session).then(
         () => {
             res.sendStatus(200);
@@ -133,7 +140,7 @@ app.post("/addSession", (req, res) => {
         });
 })
 
-app.get("/viewAllSessions", (req, res) => {
+router.get("/viewAllSessions", (req, res) => {
     sessionService.viewAllSessions().then(
         (data) => {
             res.send(data);
@@ -143,6 +150,9 @@ app.get("/viewAllSessions", (req, res) => {
         });
 })
 
+app.use('/api', router);
+
+const port = 3000;
 app.listen(port, () => {
     console.log(`TeaManager listening on port ${port}`);
 });
